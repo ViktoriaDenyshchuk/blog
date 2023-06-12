dabfa3ed900f12a33c9b35b9525379c0393d4969
ViktoriaDenyshchuk <viktoriadenyshchuk@gmail.com>
2023-06-12 20:34:20 +0300

    Лабораторна 11

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\RestTestController;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\DiggingDeeperController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,6 +20,13 @@ Route::resource('rest', RestTestController::class)->names('restTest');
 
 Route::group([ 'namespace' => 'App\Http\Controllers\Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', PostController::class)->names('blog.posts');
+});
+Route::group(['prefix' => 'digging_deeper'], function () {
+
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+
+        ->name('digging_deeper.collections');
+
 });
 //Адмінка
 $groupData = [
